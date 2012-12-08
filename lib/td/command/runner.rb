863c0f8e3a727e9f0b7915738429f94f5aed639b
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2012-12-07 19:20:42 -0800

    runner: added 'bulk_import' help message

--- lib/td/command/runner.rb
+++ lib/td/command/runner.rb
@@ -41,6 +41,7 @@ Basic commands:
   table          # create/delete/list/import/export/tail tables
   query          # issue a query
   job            # show/kill/list jobs
+  bulk_import    # manage bulk import sessions
   result         # create/delete/list result URLs
 
 Additional commands:
