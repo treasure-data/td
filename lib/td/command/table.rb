3246445d83545874df0e6d29e02f37661b01299c
Masahiro Nakagawa <repeatedly@gmail.com>
2012-12-24 10:17:34 +0900

    Add counter_updated_at support as 'Last import' in table:list

--- lib/td/command/table.rb
+++ lib/td/command/table.rb
@@ -93,14 +93,15 @@ module Command
         pschema = table.schema.fields.map {|f|
           "#{f.name}:#{f.type}"
         }.join(', ')
-        rows << {:Database => db.name, :Table => table.name, :Type => table.type.to_s, :Count => table.count.to_s, :Size => table.estimated_storage_size_string, :Schema => pschema}
+        rows << {:Database => db.name, :Table => table.name, :Type => table.type.to_s, :Count => table.count.to_s,
+          :Size => table.estimated_storage_size_string, 'Last import' => table.last_import ? table.last_import.localtime : nil, :Schema => pschema}
       }
     }
     rows = rows.sort_by {|map|
       [map[:Database], map[:Type].size, map[:Table]]
     }
 
-    puts cmd_render_table(rows, :fields => [:Database, :Table, :Type, :Count, :Size, :Schema], :max_width=>500)
+    puts cmd_render_table(rows, :fields => [:Database, :Table, :Type, :Count, :Size, 'Last import', :Schema], :max_width=>500)
 
     if rows.empty?
       if db_name
