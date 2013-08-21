1050bba8fe110825cb69b1039e42402d0a478a69
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-08-21 15:30:48 +0900

    updated td-bulk-import-java.jar: 0.2.1-SNAPSHOT to 0.2.1

--- build/update-td-bulk-import-java.sh
+++ build/update-td-bulk-import-java.sh
@@ -19,7 +19,7 @@ if [ -n "$chrev" ];then
 fi
 
 revname="$(git show --pretty=format:'%H %ad' | head -n 1)"
-vername="0.2.1-SNAPSHOT"
+vername="0.2.1"
 
 mvn package -Dmaven.test.skip=true || exit 1
 echo "copy td-bulk-import-${vername}.jar"
