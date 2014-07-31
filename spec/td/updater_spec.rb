70b970c97d7a571c8d555dc417e0f4f43307b582
Michele Caramello <michele@treasure-data.com>
2014-07-31 00:42:16 -0700

    Fix updater spec

--- spec/td/updater_spec.rb
+++ spec/td/updater_spec.rb
@@ -2,15 +2,21 @@ require 'spec_helper'
 require 'td/updater'
 
 module TreasureData::Updater
+
   describe 'without the TD_TOOLBELT_UPDATE_ROOT environment variable defined' do
+    let :default_toolbelt_url do
+      "http://toolbelt.treasuredata.com"
+    end
+
     describe 'endpoints methods' do
       it 'use the default root path' do
-        TreasureData::Updater.endpoint_root.should == TreasureData::Updater::DEFAULT_TOOLBELT_URL
-        TreasureData::Updater.version_endpoint.should =~ Regexp.new(TreasureData::Updater::DEFAULT_TOOLBELT_URL)
-        TreasureData::Updater.update_package_endpoint.should =~ Regexp.new(TreasureData::Updater::DEFAULT_TOOLBELT_URL)
+        TreasureData::Updater.endpoint_root.should == default_toolbelt_url
+        TreasureData::Updater.version_endpoint.should =~ Regexp.new(default_toolbelt_url)
+        TreasureData::Updater.update_package_endpoint.should =~ Regexp.new(default_toolbelt_url)
       end
     end
   end
+
   describe 'with the TD_TOOLBELT_UPDATE_ROOT environment variable defined' do
     before do
       ENV['TD_TOOLBELT_UPDATE_ROOT'] = 'https://0.0.0.0:5000/'
