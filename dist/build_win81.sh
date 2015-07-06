d24be5a3423187245ae784998e26ce1b1d2b1e86
takkanm <takkanm@gmail.com>
2015-07-06 17:07:08 +0900

    support TD_TOOLBELT_LOCAL_CLIENT_GEM for build_win81.sh

--- dist/build_win81.sh
+++ dist/build_win81.sh
@@ -1,5 +1,10 @@
 #! /bin/bash -u
 
+if [ $# -ne 1 ]; then
+  echo "$0 TD_TOOLBELT_LOCAL_CLIENT_GEM" 1>&2
+  exit 1
+fi
+
 bundle install
 rake exe:clean
-rake exe:build
+TD_TOOLBELT_LOCAL_CLIENT_GEM=$0 rake exe:build
