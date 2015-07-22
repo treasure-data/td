e8433b80c895489ba7dfe69a5c5f7cd651708a4b
takkanm <takkanm@gmail.com>
2015-07-22 16:27:18 +0900

    add spec import_list

--- spec/td/command/import_spec.rb
+++ spec/td/command/import_spec.rb
@@ -1,25 +1,63 @@
 require 'spec_helper'
 require 'td/command/common'
+require 'td/command/list'
 require 'td/command/import'
 
 module TreasureData::Command
   describe 'import commands' do
-    describe CommandExecutor do
-      it 'executes command' do
-        CommandExecutor.new(['echo'], nil).execute.exitstatus.should == 0
+    let(:command) {
+      Class.new { include TreasureData::Command }.new
+    }
+    let(:stdout_io) { StringIO.new }
+    let(:stderr_io) { StringIO.new }
+
+    around do |example|
+      stdout = $stdout.dup
+      stderr = $stderr.dup
+
+      begin
+        $stdout = stdout_io
+        $stderr = stderr_io
+
+        example.run
+      ensure
+        $stdout = stdout
+        $stderr = stderr
+      end
+    end
+
+    describe '#import_list' do
+      let(:option) {
+        List::CommandParser.new("import:list", [], [], nil, [], true)
+      }
+      let(:database) { 'database' }
+      let(:table)    { 'table' }
+      let(:response) {
+        [
+          double(:bulk_import,
+            name: 'bulk_import',
+            database: database,
+            table: table,
+            status: :committed,
+            upload_frozen?: true,
+            job_id: '123456',
+            valid_parts: '',
+            error_parts: '',
+            valid_records: '',
+            error_records: '',
+          )
+        ]
+      }
+
+      before do
+        client = double(:client, bulk_imports: response)
+        command.stub(:get_client).and_return(client)
+
+        command.import_list(option)
       end
 
-      it 'executes command' do
-        expect {
-          CommandExecutor.new(['exit', '1'], nil).execute
-        }.to raise_error BulkImportExecutionError, /td-bulk-import\.log/
-      end
-
-      it 'terminates process on timeout' do
-        CommandExecutor.new(['sleep', '1'], 2).execute
-        expect {
-          CommandExecutor.new(['sleep', '2'], 1).execute
-        }.to raise_error BulkImportExecutionError, /timed out/
+      it 'show import list' do
+        expect(stdout_io.string).to include [database, table].join('.')
       end
     end
   end
