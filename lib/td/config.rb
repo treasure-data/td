fd8cee9a256eb6b31f8e955cd29ccc241e7f2d4e
TAGOMORI Satoshi <tagomoris@gmail.com>
2019-12-25 15:22:09 +0900

    add ConfigFile class and use its instance for configuration file entries

--- lib/td/config.rb
+++ lib/td/config.rb
@@ -11,6 +11,11 @@ module TreasureData
   class Config
     # class variables
     @@path = ENV['TREASURE_DATA_CONFIG_PATH'] || ENV['TD_CONFIG_PATH'] || File.join(ENV['HOME'], '.td', 'td.conf')
+
+    def self.read(path=Config.path, create=false)
+      ConfigFile.new(path)
+    end
+
     @@apikey = ENV['TREASURE_DATA_API_KEY'] || ENV['TD_API_KEY']
     @@apikey = nil if @@apikey == ""
     @@cl_apikey = false # flag to indicate whether an apikey has been provided through the command-line
@@ -22,81 +27,6 @@ module TreasureData
     @@cl_import_endpoint = false # flag to indicate whether an endpoint has been provided through the command-line option
     @@secure = true
     @@retry_post_requests = false
-
-    def initialize
-      @path = nil
-      @conf = {}   # section.key = val
-    end
-
-    def self.read(path=Config.path, create=false)
-      new.read(path)
-    end
-
-    def [](cate_key)
-      @conf[cate_key]
-    end
-
-    def []=(cate_key, val)
-      @conf[cate_key] = val
-    end
-
-    def delete(cate_key)
-      @conf.delete(cate_key)
-    end
-
-    def read(path=@path)
-      @path = path
-      begin
-        data = File.read(@path)
-      rescue
-        e = ConfigNotFoundError.new($!.to_s)
-        e.set_backtrace($!.backtrace)
-        raise e
-      end
-
-      section = ""
-
-      data.each_line {|line|
-        line.strip!
-        case line
-        when /^#/
-          next
-        when /\[(.+)\]/
-          section = $~[1]
-        when /^(\w+)\s*=\s*(.+?)\s*$/
-          key = $~[1]
-          val = $~[2]
-          @conf["#{section}.#{key}"] = val
-        else
-          raise ConfigParseError, "invalid config line '#{line}' at #{@path}"
-        end
-      }
-
-      self
-    end
-
-    def save(path=@path||Config.path)
-      @path = path
-      write
-    end
-
-    private
-    def write
-      require 'fileutils'
-      FileUtils.mkdir_p File.dirname(@path)
-      File.open(@path, "w") {|f|
-        @conf.keys.map {|cate_key|
-          cate_key.split('.', 2)
-        }.zip(@conf.values).group_by {|(section,key), val|
-          section
-        }.each {|section,cate_key_vals|
-          f.puts "[#{section}]"
-          cate_key_vals.each {|(section,key), val|
-            f.puts "  #{key} = #{val}"
-          }
-        }
-      }
-    end
 
     def self.path
       @@path
@@ -193,5 +123,78 @@ module TreasureData
       string
     end
 
+    class ConfigFile
+      def initialize(path=nil)
+        @path = path
+        @conf = {}   # section.key = val
+        read if path
+      end
+
+      def [](cate_key)
+        @conf[cate_key]
+      end
+
+      def []=(cate_key, val)
+        @conf[cate_key] = val
+      end
+
+      def delete(cate_key)
+        @conf.delete(cate_key)
+      end
+
+      def read(path=@path)
+        @path = path
+        begin
+          data = File.read(@path)
+        rescue
+          e = ConfigNotFoundError.new($!.to_s)
+          e.set_backtrace($!.backtrace)
+          raise e
+        end
+
+        section = ""
+
+        data.each_line {|line|
+          line.strip!
+          case line
+          when /^#/
+            next
+          when /\[(.+)\]/
+            section = $~[1]
+          when /^(\w+)\s*=\s*(.+?)\s*$/
+            key = $~[1]
+            val = $~[2]
+            @conf["#{section}.#{key}"] = val
+          else
+            raise ConfigParseError, "invalid config line '#{line}' at #{@path}"
+          end
+        }
+
+        self
+      end
+
+      def save(path=@path||Config.path)
+        @path = path
+        write
+      end
+
+      private
+      def write
+        require 'fileutils'
+        FileUtils.mkdir_p File.dirname(@path)
+        File.open(@path, "w") {|f|
+          @conf.keys.map {|cate_key|
+            cate_key.split('.', 2)
+          }.zip(@conf.values).group_by {|(section,key), val|
+            section
+          }.each {|section,cate_key_vals|
+            f.puts "[#{section}]"
+            cate_key_vals.each {|(section,key), val|
+              f.puts "  #{key} = #{val}"
+            }
+          }
+        }
+      end
+    end
   end # class Config
 end # module TreasureData
